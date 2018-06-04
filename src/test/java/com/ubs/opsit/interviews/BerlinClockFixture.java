70568afb730621e63ad535eb0acd2cd8699ac4c5
khanwilkarreena <39836006+khanwilkarreena@users.noreply.github.com>
2018-06-04 20:17:55 +0530

    Add files via upload

--- src/test/java/com/ubs/opsit/interviews/BerlinClockFixture.java
+++ src/test/java/com/ubs/opsit/interviews/BerlinClockFixture.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class BerlinClockFixture {
 
-    private TimeConverter berlinClock;
+    private BerlinClockImpl berlinClock = new BerlinClockImpl();
     private String theTime;
 
     @Test
